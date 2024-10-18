1dd57286ac2540b44e23d182eea50d32e4c27a56
Clemssandra2939 <sclems600@gmail.com>
2024-10-18 11:27:24 +0100

    fizzbuzz.py

--- Adding_Even_Num.py
+++ Adding_Even_Num.py
@@ -1,6 +1,26 @@
 # Adding even  number from 1 to 100 including 1 and 100
 
-total_numbr = 0
-for numbr in range (2, 101,2):
-    total_numbr += numbr
-print(total_numbr)
+# total_numbr = 0
+# for numbr in range (2, 101,2):
+#     total_numbr += numbr
+# print(total_numbr)
+
+
+total_number = 0
+for number in range (1,101):
+    if number % 2==0:
+        total_number += number
+print(total_number)
+
+
+
+# Fizz Buzz coding char
+# You are going to write a program that automatically print the solution
+# to the fizz buzz game
+
+# Your program should print each number fro 1 to 100 in turn
+# when the number is divided by 3 then instead of printing the number,it should print "fizz"
+#  when the number is divided by 5 then instead of printing the number,it should print "buzz"
+# And if the number is divided by 3 and 5 instead of printing the number,it should print "Fizz Buzz"
+
+
